591b3f44b9a284efadd7be53c273251fbe645727
Hakan Yildiz <hakan@re-coded.com>
2020-04-09 11:58:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function createEmployeeRecord(employeeInfo){
+  return {
+  firstName: employeeInfo[0],
+  familyName: employeeInfo[1],
+  title: employeeInfo[2],
+  payPerHour: employeeInfo[3],
+  timeInEvents:[],
+  timeOutEvents:[]
+  }
+}
+
+function createEmployeeRecords(recordArrays){
+  return recordArrays.map(recordArray=>createEmployeeRecord(recordArray))
+}
+
+/*it("creates the correct type", function () {
+        let bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3])
+        let updatedBpRecord = createTimeInEvent(bpRecord, "2014-02-28 1400")
+        let newEvent = updatedBpRecord.timeInEvents[0]
+        expect(newEvent.type).to.equal("TimeIn")
+      })*/
+
+function createTimeEvent(timeInObj){
+  let 
+}
